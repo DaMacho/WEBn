c904fd1a17bd3a003506a467ce5f631d4f4a0b90
DaMacho <po4865@gmail.com>
2018-09-11 17:51:29 +0900

    Seperated Body and Links color setter JavaScript codes from 3.html. Seperated color setters JavaScript code of Body and Links from 3.html to color.js

---
+++ colors.js
@@ -0,0 +1,37 @@
+var Body = {
+    setBackgroundColor: function (color) {
+        document.querySelector('body').style.backgroundColor = color;
+    },
+    setColor: function (color) {
+        document.querySelector('body').style.color = color;
+    }
+}
+
+var Links = {
+    setColor: function (color) {
+        var alist = document.querySelectorAll('a');
+        var i = 0;
+        while (i < alist.length) {
+            alist[i].style.color = color;
+            i++;
+        }
+    }
+}
+
+function nightDayHandler(self) {
+    if (self.value === 'night') {
+        Body.setBackgroundColor('black');
+        Body.setColor('white');
+
+        Links.setColor('powderblue');
+
+        self.value = 'day';
+    } else {
+        Body.setBackgroundColor('white');
+        Body.setColor('black');
+
+        Links.setColor('blue');
+
+        self.value = 'night';
+    }
+}
