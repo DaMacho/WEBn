f25feb6ddba60a35bf06f2f99917c526944ccd92
DaMacho <po4865@gmail.com>
2018-09-21 00:06:07 +0900

    functionized HTML components into func 'templateHTML' and 'templateList'. The function 'templateHTML' take input of title of contents, list of contents, and description of contents and return html document of page. The function templateList take filelist as input, which is list of files from data directory, and generates list of contents from directory.

--- main.js
+++ main.js
@@ -49,40 +49,10 @@ var app = http.createServer(function (request, response) {
         if (queryData.id === undefined) {
             title = 'WEB';
         }
-
         fs.readdir('data', function(error, filelist){
-            // var list = `<ul>`;
-            // var i = 0;
-            // while(i < filelist.length-1){
-            //     list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-            //     i++
-            // }
-            // list = list+`</ul>`;
-
             fs.readFile(`data/${title}`, 'utf-8', function(err, description){
                 var list = templateList(filelist);
                 var template = templateHTML(title, list, description);
-                // var template = `
-                // <!DOCTYPE html>
-                // <html>
-                // <head>
-                //     <meta charset="UTF-8">
-                //     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                //     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-                //     <title>WEBn - ${title}</title>
-                // </head>
-                // <body>
-                //     <h1><a href="/">WEB</a></h1>
-                //     <div>
-                //         ${list}
-                //         <div>
-                //             <h2>${title}</h2>
-                //             <p>${description}</p>
-                //         </div>
-                //     </div>
-                // </body>
-                // </html>
-                // `;
                 response.writeHead(200);
                 response.end(template);
             })
