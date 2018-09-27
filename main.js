57acc955b15e25aab29f7063dc7c262c655521db
DaMacho <po4865@gmail.com>
2018-09-27 23:49:04 +0900

    made data transfer process with POST method made data transfer process for creating article with POST method.

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
 
 function templateList(filelist){
     var list = `<ul>`;
@@ -60,7 +61,7 @@ var app = http.createServer(function (request, response) {
             title = 'WEB - create';
             var list = templateList(filelist);
             var template = templateHTML(title, list, `
-                <form action="http://localhost:3000/process_create" method="post">
+                <form action="http://localhost:3000/create_process" method="post">
                     <p>
                         <input type="text" name="title" placeholder="title">
                     </p>
@@ -75,6 +76,18 @@ var app = http.createServer(function (request, response) {
             response.writeHead(200);
             response.end(template);
         });
+    } else if (pathname === '/create_process') {
+        var body = '';
+        request.on('data', function(data){
+            body = body + data;
+        });
+        request.on('end', function(){
+            var post = qs.parse(body);
+            var title = post.title;
+            var description = post.description;
+        });
+        response.writeHead(200);
+        response.end('success');
     } else {
         response.writeHead(404);
         response.end('Page not found');
