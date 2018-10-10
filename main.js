e0e38d8c886243c98017131a332ec3955a1ab88f
DaMacho <po4865@gmail.com>
2018-10-10 16:38:14 +0900

    edited main.js to prevent potential security issue edited main.js to prevent potential security issue when taking input from client through url using id with directory hacking.

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
 var template = require('./lib/template.js');
+var path = require('path');
 
 var app = http.createServer(function (request, response) {
     var _url = request.url;
@@ -15,7 +16,8 @@ var app = http.createServer(function (request, response) {
             var title = 'WEB';
         }
         fs.readdir('data', function(error, filelist){
-            fs.readFile(`data/${title}`, 'utf-8', function(err, description){
+            var filteredId = path.parse(title).base;
+	    fs.readFile(`data/${filteredId}`, 'utf-8', function(err, description){
                 var list = template.list(filelist);
                 var html= template.HTML(title, list, description, 
                     `
@@ -67,7 +69,8 @@ var app = http.createServer(function (request, response) {
         });
     } else if (pathname === '/update') {
         fs.readdir('./data', function(error, filelist){
-            fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
+	    var filteredId = path.parse(title).base;
+            fs.readFile(`data/${filteredId}`, 'utf8', function(err, description) {
                 var list = template.list(filelist);
                 var html= template.HTML(title, list, 
                 `
@@ -113,7 +116,8 @@ var app = http.createServer(function (request, response) {
         request.on('end', function(){
             var post = qs.parse(body);
             var id = post.id;
-            fs.unlink(`data/${id}`, function(error){
+	    var filteredId = path.parse(id).base;
+            fs.unlink(`data/${filteredId}`, function(error){
                 response.writeHead(302, {Location: `/`});
                 response.end();
             });
