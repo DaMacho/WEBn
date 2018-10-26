23f1ced73dd43a7e8795255b50316f81bf9eda2b
DaMacho <po4865@gmail.com>
2018-10-26 12:50:08 +0900

    modified main.js & template.js to add author option into CREATE page.

--- main.js
+++ main.js
@@ -68,23 +68,30 @@ var app = http.createServer(function (request, response) {
         }
     } else if (pathname === '/create') {
         db.query(`SELECT * FROM topic`, function(error, topics){
-            title = 'WEB - create';
-            var list = template.list(topics);
-            var html= template.HTML(title, list, `
-                <form action="/create_process" method="post">
-                    <p>
-                        <input type="text" name="title" placeholder="title">
-                    </p>
-                    <p>
-                        <textarea name="description" placeholder="description"></textarea>
-                    </p>
-                    <p>
-                        <input type="submit">
-                    </p>
-                </form>
-            `, '');
-            response.writeHead(200);
-            response.end(html);
+            db.query(`SELECT * FROM author`, function(error2, authors){
+                title = 'WEB - create';
+                var list = template.list(topics);
+                var html= template.HTML(title, list, `
+                    <form action="/create_process" method="post">
+                        <p>
+                            <input type="text" name="title" placeholder="title">
+                        </p>
+                        <p>
+                            <textarea name="description" placeholder="description"></textarea>
+                        </p>
+                        <p>
+                            ${template.authorSelect(authors)}
+                        </p>
+                        <p>
+                            <input type="submit">
+                        </p>
+                    </form>
+                    `, 
+                    `<a href="/create">create</a>`
+                );
+                response.writeHead(200);
+                response.end(html);
+            });
         });
     } else if (pathname === '/create_process') {
         var body = '';
@@ -95,7 +102,7 @@ var app = http.createServer(function (request, response) {
             var post = qs.parse(body);
             db.query(
                 `INSERT INTO topic (title, description, created, author_id) VALUES(?, ?, NOW(), ?)`, 
-                [post.title, post.description, 1], 
+                [post.title, post.description, post.author], 
                 function(error, result){
                     if (error) {
                         throw error;
