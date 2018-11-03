9902ef3991a7081d2b67d6de109cad211f5cb1d7
DaMacho <po4865@gmail.com>
2018-11-03 11:59:47 +0900

    made & applied middleware that read filelist from directory for GET process

--- main_express.js
+++ main_express.js
@@ -12,9 +12,17 @@ const app = express()
 // process POST request
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(compression())
+app.get('*', function(req, res, next){
+    fs.readdir('./data', (error, filelist) => {
+        req.list = filelist
+        next()
+    })
+})
+
 
 // route, routing
 app.get('/', (req, res) => {
+    /*
     fs.readdir('./data', (error, filelist) => {
         const title = 'Welcome'
         const description = 'Hello, Node.js & Express'
@@ -24,52 +32,57 @@ app.get('/', (req, res) => {
         )
         res.send(html)
     })
+    */
+    const title = 'Welcome'
+    const description = 'Hello, Node.js & Express'
+    const list = template.list(req.list)
+    const html = template.HTML(title, list, description,
+        `<a href="/create">create</a>`
+    )
+    res.send(html)
 })
 
 app.get('/page/:pageId', (req, res) => {
-    fs.readdir('data', (error, filelist) => {
-        const filteredId = path.parse(req.params.pageId).base
-        fs.readFile(`data/${filteredId}`, 'utf-8', (err, description) => {
-            const title = req.params.pageId
-            const sanitizedTitle = sanitizeHtml(title)
-            const sanitizedDescription = sanitizeHtml(description, {
-                allowedTags:['h1']
-            })
-            const list = template.list(filelist)
-            const html= template.HTML(sanitizedTitle, list, sanitizedDescription, 
-                `
-                <a href="/create">create</a>
-                <a href="/update/${sanitizedTitle}">update</a>
-                <form action="/delete_process" method="POST">
-                    <input type="hidden" name="id" value="${sanitizedTitle}">
-                    <input type="submit" value="delete">
-                </form>
-                `
-            )
-            res.send(html)
+    const filteredId = path.parse(req.params.pageId).base
+    fs.readFile(`data/${filteredId}`, 'utf-8', (err, description) => {
+        const title = req.params.pageId
+        const sanitizedTitle = sanitizeHtml(title)
+        const sanitizedDescription = sanitizeHtml(description, {
+            allowedTags:['h1']
         })
+        const list = template.list(req.list)
+        const html= template.HTML(sanitizedTitle, list, sanitizedDescription, 
+            `
+            <a href="/create">create</a>
+            <a href="/update/${sanitizedTitle}">update</a>
+            <form action="/delete_process" method="POST">
+                <input type="hidden" name="id" value="${sanitizedTitle}">
+                <input type="submit" value="delete">
+            </form>
+            `
+        )
+        res.send(html)
     })
+    console.log(req)
 })
 
 app.get('/create', (req, res) => {
-    fs.readdir('./data', (error, filelist) => {
-        title = 'WEB - create'
-        var list = template.list(filelist)
-        var html= template.HTML(title, list, `
-            <form action="/create_process" method="post">
-                <p>
-                    <input type="text" name="title" placeholder="title">
-                </p>
-                <p>
-                    <textarea name="description" placeholder="description"></textarea>
-                </p>
-                <p>
-                    <input type="submit">
-                </p>
-            </form>
-        `, '')
-        res.send(html)
-    })
+    title = 'WEB - create'
+    var list = template.list(req.list)
+    var html= template.HTML(title, list, `
+        <form action="/create_process" method="post">
+            <p>
+                <input type="text" name="title" placeholder="title">
+            </p>
+            <p>
+                <textarea name="description" placeholder="description"></textarea>
+            </p>
+            <p>
+                <input type="submit">
+            </p>
+        </form>
+    `, '')
+    res.send(html)
 })
 
 app.post('/create_process', (req, res) => {
@@ -92,34 +105,32 @@ app.post('/create_process', (req, res) => {
     const title = post.title
     const description = post.description
     fs.writeFile(`data/${title}`, description, 'utf8', (err) => {
-        res.writeHead(302, {Location: `/?id=${title}`})
+        res.writeHead(302, {Location: `/page/${title}`})
         res.end()
     })
 })
 
 app.get('/update/:pageId', (req, res) => {
-    fs.readdir('./data', (error, filelist) => {
-	    const filteredId = path.parse(req.params.pageId).base
-        fs.readFile(`data/${filteredId}`, 'utf8', (err, description) => {
-            const title = req.params.pageId
-            const list = template.list(filelist)
-            const html= template.HTML(title, list, 
-            `
-            <form action="/update_process" method="post">
-                <input type="hidden" name="id" value="${title}">
-                <p><input type="text" name="title" placeholder="title" value="${title}"</p>
-                <p>
-                    <textarea name="description" placeholder="description">${description}</textarea>
-                </p>
-                <p>
-                    <input type="submit">
-                </p>
-            </form>
-            `,
-            `<a href="/create">create</a> <a href="/update/${title}">update</a>`
-            )
-            res.send(html)
-        })
+    const filteredId = path.parse(req.params.pageId).base
+    fs.readFile(`data/${filteredId}`, 'utf8', (err, description) => {
+        const title = req.params.pageId
+        const list = template.list(req.list)
+        const html= template.HTML(title, list, 
+        `
+        <form action="/update_process" method="post">
+            <input type="hidden" name="id" value="${title}">
+            <p><input type="text" name="title" placeholder="title" value="${title}"</p>
+            <p>
+                <textarea name="description" placeholder="description">${description}</textarea>
+            </p>
+            <p>
+                <input type="submit">
+            </p>
+        </form>
+        `,
+        `<a href="/create">create</a> <a href="/update/${title}">update</a>`
+        )
+        res.send(html)
     })
 })
 
